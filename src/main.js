a406f9f6be372d642605532165b4a858fbae27ea
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-10-23 12:09:35 +0300

    izitoast added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
 import fetchPic from './js/pixabay-api';
 
 const form = document.querySelector('.form');
